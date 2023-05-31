19a355b0f18eacd64a449202708516b41c127d5a
fuhao <1627765128@qq.com>
2023-05-31 14:36:32 +0800

    message

--- dataset/nba_data_dynamic.py
+++ dataset/nba_data_dynamic.py
@@ -3,6 +3,7 @@ import csv
 import json
 import math
 
+
 def get_data(url):
         headers={
                 'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42',
@@ -71,8 +72,21 @@ def sort(playerList,player_ranking):
                 rank = math.ceil((i+1)/5)
                 item['RANKING'] = str(rank)
                 playerList_sort.append(item)
-    print(playerList_sort)
+    # print(playerList_sort)
     return playerList_sort
+
+def delate(playerList):
+    # 统计None出现次数，大于4删除该球员信息
+    player_delate = []
+    for i in range (len(playerList)):
+        c = 0
+        item = playerList[i]
+        for key in item:
+            if (item[key] == None):
+                c += 1
+        if (c<=4):
+            player_delate.append(item)
+    return player_delate
 
 
 def write_data(filename,playerList_sort):
@@ -84,20 +98,73 @@ def write_data(filename,playerList_sort):
             write.writerow(data)
     return 0
 
+
+def count(playerList):
+    count_ = 0
+    count_effective = 0
+    for i in range (len(playerList)):
+        item = playerList[i]
+        for key in item:
+            count_ += 1
+            if (item[key] != None):
+                count_effective += 1
+    return count_, count_effective
+
 # url = 'https://stats.nba.com/stats/draftcombinedrillresults?LeagueID=00&SeasonYear=2022-23'
 # url_ranking = 'https://stats.nba.com/stats/drafthistory?College=&LeagueID=00&OverallPick=&RoundNum=&RoundPick=&Season=2022&TeamID=0&TopX='
+
+num_dynamic_total = 0
+num_ranking = 0
+
+total_count = 0
+total_count_effective = 0
+
+playerList_delate_all = []
 
 for i in range(2013,2023):
         url = 'https://stats.nba.com/stats/draftcombinedrillresults?LeagueID=00&SeasonYear='+str(i)+'-'+str(i-1999)
         url_ranking = 'https://stats.nba.com/stats/drafthistory?College=&LeagueID=00&OverallPick=&RoundNum=&RoundPick=&Season='+str(i)+'&TeamID=0&TopX='
         json_data = get_data(url)
         json_data_ranking = get_data(url_ranking)
+        # 全部动态体测数据
         playerList = player_data(json_data)
+        # 对应年份排名数据
         player_ranking = player_data_ranking(json_data_ranking)
+        # 结合排名和动态体测数据
         playerList_sort = sort(playerList,player_ranking)
-        filename = 'nba_player_dynamic measurement data-'+str(i)+'.csv'
-        print(len(playerList_sort),len(player_ranking))
+        # 删除数据缺失的NBA球员
+        playerList_delate = delate(playerList_sort)
+
+        for data in playerList_delate:
+            playerList_delate_all.append(data)
+
+        # 动态数据总人数
+        num_dynamic_total += len(playerList)
+        # 拥有排名人数
+        num_ranking += len(playerList_sort)
+
+        # 有排名的NBA球员的全部动态体测数据和有效动态体测数据
+        count_, count_effective = count(playerList_sort)
+        total_count += count_
+        total_count_effective += count_effective
+
+        filename = 'nba_player_dynamic measurement data-' + str(i) + '.csv'
         write_data(filename, playerList_sort)
 
-#
+filename_delate = 'nba_player_dynamic measurement data all.csv'
+write_data(filename_delate, playerList_delate_all)
+
+print("爬取动态数据总人数：",num_dynamic_total," 拥有排名人数：",num_ranking)
+category = len(playerList_sort[0])
+print("类别总数：",category)
+print("NBA球员的全部动态体测数据：",total_count," 和有效动态体测数据：",total_count_effective)
+
+
+
+
+
+
+
+
+
 
--- dataset/nba_data_static.py
+++ dataset/nba_data_static.py
@@ -82,8 +82,22 @@ def sort(playerList,player_ranking):
                 rank = math.ceil((i+1)/5)
                 item['RANKING'] = str(rank)
                 playerList_sort.append(item)
-    print(playerList_sort)
+    # print(playerList_sort)
     return playerList_sort
+
+def delate(playerList):
+    # 统计None出现次数，大于4删除该球员信息
+    player_delate = []
+    for i in range (len(playerList)):
+        c = 0
+        item = playerList[i]
+        for key in item:
+            if (item[key] == None):
+                c += 1
+        if (c<=4):
+            player_delate.append(item)
+    return player_delate
+
 
 
 def write_data(filename,playerList_sort):
@@ -95,21 +109,65 @@ def write_data(filename,playerList_sort):
             write.writerow(data)
     return 0
 
+def count(playerList):
+    count_ = 0
+    count_effective = 0
+    for i in range (len(playerList)):
+        item = playerList[i]
+        for key in item:
+            count_ += 1
+            if (item[key] != None):
+                count_effective += 1
+    return count_, count_effective
+
 
 
 # url = 'https://stats.nba.com/stats/draftcombineplayeranthro?LeagueID=00&SeasonYear=2022-23'
 # url_ranking = 'https://stats.nba.com/stats/drafthistory?College=&LeagueID=00&OverallPick=&RoundNum=&RoundPick=&Season=2022&TeamID=0&TopX='
+
+num_static_total = 0
+num_ranking = 0
+
+total_count = 0
+total_count_effective = 0
+
+playerList_delate_all = []
 
 for i in range(2013,2023):
         url = 'https://stats.nba.com/stats/draftcombineplayeranthro?LeagueID=00&SeasonYear='+str(i)+'-'+str(i-1999)
         url_ranking = 'https://stats.nba.com/stats/drafthistory?College=&LeagueID=00&OverallPick=&RoundNum=&RoundPick=&Season='+str(i)+'&TeamID=0&TopX='
         json_data = get_data(url)
         json_data_ranking = get_data(url_ranking)
+        # 全部静态体测数据
         playerList = player_data(json_data)
+        # 对应年份排名数据
         player_ranking = player_data_ranking(json_data_ranking)
-        playerList_sort = sort(playerList,player_ranking)
-        filename = 'nba_player_static measurement data-'+str(i)+'.csv'
-        print(len(playerList_sort),len(player_ranking))
+        # 结合排名和静态体测数据
+        playerList_sort = sort(playerList, player_ranking)
+        # 删除数据缺失的NBA球员
+        playerList_delate = delate(playerList_sort)
+
+        for data in playerList_delate:
+            playerList_delate_all.append(data)
+
+        # 静态数据总人数
+        num_static_total += len(playerList)
+        # 拥有排名人数
+        num_ranking += len(playerList_sort)
+
+        # 有排名的NBA球员的全部静态体测数据和有效静态体测数据
+        count_, count_effective = count(playerList_sort)
+        total_count += count_
+        total_count_effective += count_effective
+
+        filename = 'nba_player_static measurement data-' + str(i) + '.csv'
         write_data(filename, playerList_sort)
 
 
+filename_delate = 'nba_player_static measurement data all.csv'
+write_data(filename_delate, playerList_delate_all)
+
+print("爬取静态数据总人数：",num_static_total," 拥有排名人数：",num_ranking)
+category = len(playerList_sort[0])
+print("类别总数：",category)
+print("NBA球员的全部静态体测数据：",total_count," 和有效静态体测数据：",total_count_effective)
